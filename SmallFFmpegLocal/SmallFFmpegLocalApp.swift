1103b2dc30c763dbf2ccfd61cfbcc9954e735c29
Jose Vigil <josemanuelvigil@gmail.com>
2024-12-05 01:01:21 -0300

    Initial Commit

---
+++ SmallFFmpegLocal/SmallFFmpegLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallFFmpegLocalApp.swift
+//  SmallFFmpegLocal
+//
+//  Created by Jose Vigil on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SmallFFmpegLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
